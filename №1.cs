2c9a34fc651b9ded0606fc978438899c50bea067
Anfffna <annan_nikolaevna2004@mail.ru>
2024-10-11 12:09:03 +0300

    КР №1.1 (вар. шрифты 1)

---
+++ №1.cs
@@ -0,0 +1,16 @@
+﻿Console.WriteLine("Введите текст для шифрования:");
+string input = Console.ReadLine();
+Console.WriteLine("Зашифрованный текст: " + Encrypt(input));
+
+    static string Encrypt(string text)
+{
+    const string alphabet = "АБВГДЕЁЖЗИЙКЛМНОПРСТУФХЦЧШЩЪЫЬЭЮЯ";
+    char[] result = new char[text.Length];
+
+    for (int i = 0; i < text.Length; i++)
+    {
+        int index = alphabet.IndexOf(text[i]);
+        result[i] = index >= 0 ? alphabet[alphabet.Length - 1 - index] : text[i];
+    }
+    return new string(result);
+}

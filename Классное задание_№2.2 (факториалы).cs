0683dea527411520344462d79a3d73f60204a123
Anfffna <annan_nikolaevna2004@mail.ru>
2024-10-14 14:20:32 +0300

    Классное задание_№2.2 (факториалы)

---
+++ Классное задание_№2.2 (факториалы).cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        // Функция для вычисления факториала
+        double Factorial(int n)
+        {
+            double result = 1;
+            for (int i = 2; i <= n; i++)
+                result *= i;
+            return result;
+        }
+
+        Console.Write("Введите число: ");
+        double x = Convert.ToDouble(Console.ReadLine());
+
+        // Вычисление ряда Тейлора для sin(x)
+        double y = 0;
+        for (int n = 0; n < 7; n++)
+        {
+            int exp = 2 * n + 1;
+            double term = Math.Pow(x, exp) / Factorial(exp);
+            y += (n % 2 == 0 ? term : -term);  // Чередование знаков
+        }
+
+        // Вывод
+        Console.WriteLine($"sin({x}) ≈ {y}");
+    }
+}
